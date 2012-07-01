40fe8b452129ec6a2d7af7e7a26c33307a1ec17c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-07-01 16:01:27 +1000

    Ready

--- server.node.js
+++ server.node.js
@@ -5,18 +5,4 @@ var gateway = require('ncombo/core/gateway');
 var ncombo = require('ncombo'),
 	fs = require('fs');
 
-gateway.watchForbidden(function() {
-	console.log('forbidden');
-});
-
-var prep = function(siRequest) {
-	//gateway.restrict();
-	//gateway.allow('test', 'fun');
-	console.log('prepare', siRequest.getServerInterface(), siRequest.getMethod(), siRequest.getData());
-}
-
-ncombo.prepare(prep);
-
-//ncombo.useStyle('node_modules/ncombo/styles/bootstrap.less', 'text/css', 'stylesheet');
-
 ncombo.start(port, {release: false});

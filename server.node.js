a13f8daecbb9a709bc60ba99ce7ffcf2f80bd2fd
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-10-02 00:55:24 +1000

    Improved memory management to account for unexpected IO errors

--- server.node.js
+++ server.node.js
@@ -1,6 +1,6 @@
-var arguments = process.argv.splice(2);
+var arguments = process.argv.slice(2);
 var port = arguments[0] ? arguments[0] : 8000;
 
 var ncombo = require('ncombo');
 
-ncombo.start(port, {release: false});
+ncombo.start({port: port, release: false});

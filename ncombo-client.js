dbfea2a4ad046fd6d693ad1016bba37db2e405de
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-12-08 11:32:24 +1000

    Updated mixin util function.

--- ncombo-client.js
+++ ncombo-client.js
@@ -99,31 +99,49 @@ var $n = {
 		
 		proto.initMixin = function(MixinClass) {
 			var args = Array.prototype.slice.call(arguments, 1);
+			var i, value;
 			
+			var protoClone = {};
+			for(i in proto) {
+				protoClone[i] = proto[i];
+			}
+			
+			for(i in MixinClass.prototype) {
+				this[i] = MixinClass.prototype[i];
+			}
+			
+			// Using different calls for browser compatibility reasons
 			if(args) {
 				MixinClass.apply(this, args);
 			} else {
 				MixinClass.apply(this);
 			}
 			
-			var mixinInstance = {};
-			proto.__internalMixinMethods[MixinClass] = mixinInstance;
+			var mixinMethods = {};
 			
-			var i, value;
 			for(i in this) {
 				value = this[i];
 				if(value instanceof Function) {
-					mixinInstance[i] = value;
+					mixinMethods[i] = value;
 				}
 			}
+			
+			for(i in protoClone) {
+				value = protoClone[i];
+				if(i != '__internalMixinMethods') {
+					this[i] = value;
+				}
+			}
+			
+			this.__internalMixinMethods[MixinClass] = mixinMethods;
 		}
 		
 		proto.callMixinMethod = function(MixinClass, method) {
 			var args = Array.prototype.slice.call(arguments, 2);
 			if(args) {
-				return proto.__internalMixinMethods[MixinClass][method].apply(this, args);
+				return this.__internalMixinMethods[MixinClass][method].apply(this, args);
 			} else {
-				return proto.__internalMixinMethods[MixinClass][method].apply(this);
+				return this.__internalMixinMethods[MixinClass][method].apply(this);
 			}
 		}
 		
@@ -131,11 +149,11 @@ var $n = {
 			if(args && !(args instanceof Array)) {
 				throw 'Exception: The args parameter of the applyMixinMethod function must be an Array';
 			}
-			return proto.__internalMixinMethods[MixinClass][method].apply(this, args);
+			return this.__internalMixinMethods[MixinClass][method].apply(this, args);
 		}
 		
 		proto.instanceOf = function(classReference) {
-			return this instanceof classReference || proto.__internalMixinMethods.hasOwnProperty(classReference);
+			return this instanceof classReference || this.__internalMixinMethods.hasOwnProperty(classReference);
 		}
 		
 		return MainClass;

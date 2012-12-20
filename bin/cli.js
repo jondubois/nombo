5817dca9d86d36cd1e37b68d9ac0d69c0c663db5
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-12-21 01:36:48 +1000

    Minor change

--- bin/cli.js
+++ bin/cli.js
@@ -44,7 +44,8 @@ var showCorrectUsage = function() {
 	console.log('Usage: ncombo [OPTIONS]');
 	console.log('Install nCombo in current directory\n');
 	console.log('Available options:');
-	console.log('-s                   Generate a directory containing sample ncombo apps');
+	console.log('-v                   Get the current version number of your nCombo installation');
+	console.log('-s                   Generate a directory containing sample nCombo apps');
 	console.log('--help               Get info on how to use this command');
 	console.log('--force              Force all necessary directory modifications without prompts');
 }

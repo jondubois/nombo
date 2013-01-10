355d9d35bf3ad11871c1779045f9570e11434c7f
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2013-01-10 22:39:19 +1000

    Do not allow refreshing scripts while in release mode

--- samples/multiple_scripts/scripts/c.js
+++ samples/multiple_scripts/scripts/c.js
@@ -2,6 +2,6 @@
 
 exports.ClassC = function() {
 	this.getMessage = function() {
-		return 'This is getMessage()method of ClassC from script c';
+		return 'This is getMessage() method of ClassC from script c';
 	}
 }

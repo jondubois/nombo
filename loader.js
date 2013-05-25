5d866fce2a34bc10599b8aff1d24a4a9e2fe6f9d
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2013-05-25 14:46:15 +1000

    Smooth progress in IE

--- loader.js
+++ loader.js
@@ -302,7 +302,7 @@ var $loader = {
 		}
 	
 		var xmlhttp = $loader._getHTTPReqObject();
-		if(settings.progress && xmlhttp.hasOwnProperty('onprogress') && navigator.userAgent.indexOf("Opera") < 0) {
+		if(settings.progress && navigator.userAgent.indexOf("Opera") < 0) {
 			xmlhttp.onprogress = settings.progress;
 		}
 		xmlhttp.open(type, settings.url, true);
